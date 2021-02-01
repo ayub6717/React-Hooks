0aa779a73eb088b2bf26f0e8e9e040532edd0069
ayub6717 <ayub15-6717@diu.edu.bd>
2021-02-01 15:13:05 +0600

    effect only Once

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
-import HookCounter from './Components/HookCounter';
-import HookCounterFour from './Components/HookCounterFour';
-import HookCounterTree from './Components/HookCounterTree';
-import HookCounterTwo from './Components/HookCounterTwo';
-import HookUseEffect from './Components/HookUseEffect';
+// import HookCounter from './Components/HookCounter';
+// import HookCounterFour from './Components/HookCounterFour';
+// import HookCounterTree from './Components/HookCounterTree';
+// import HookCounterTwo from './Components/HookCounterTwo';
+// import HookUseEffect from './Components/HookUseEffect';
+import HookMouse from './Components/HookMouse';
 
 function App() {
   return (
     <div className="App">
-      <HookCounter /> <br/>
+      {/* <HookCounter /> <br/>
       <HookCounterTwo /> <br/>
       <HookCounterTree /> <br/>
       <HookCounterFour /> <br/>
-      <HookUseEffect />
+      <HookUseEffect /> <br/> */}
+      <HookMouse />
     </div>
   );
 }
---
+++ src/Components/HookMouse.js
@@ -0,0 +1,24 @@
+import React, {useState, useEffect} from 'react'
+
+function HookMouse() {
+    const [x, setX] = useState(0)
+    const [y, setY] = useState(0)
+
+    const logMousePosition = e => {
+        console.log('Mouse event')
+        setX(e.clientX)
+        setY(e.clientY)
+    }
+
+    useEffect(()=> {
+        console.log( 'useEffect called')
+        window.addEventListener('Mousemove', logMousePosition)
+    }, [])
+    return (
+        <div>
+           <h3> Hooks X - {x} Y - {y} </h3>
+        </div>
+    )
+}
+
+export default HookMouse
