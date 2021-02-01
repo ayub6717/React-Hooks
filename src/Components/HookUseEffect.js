85725f1c7e168ddbea8c132ffa4801182ebab7cb
ayub6717 <ayub15-6717@diu.edu.bd>
2021-02-01 13:00:25 +0600

    Conditionally run effects

--- src/Components/HookUseEffect.js
+++ src/Components/HookUseEffect.js
@@ -2,12 +2,15 @@ import React, {useEffect, useState} from 'react'
 
 function HookUseEffect() {
     const [count, setCount] = useState(0)
+    const [name, setName] = useState('')
 
     useEffect (() => {
+        console.log(`useEffect- updating value`)
         document.title= `You Click ${count} Times`
-    })
+    }, [count])
     return (
         <div>
+            <input type="text" value={name} onChange={e => setName(e.target.value)} />
             <button onClick={()=> setCount(count+1)}>
                 click me {count}
             </button>
